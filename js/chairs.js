e01b7dec8c5f9d3c8fc6e09da5bf031ef1962ac0
Daniel Dropik <ddropik@wisc.edu>
2016-02-22 14:57:19 -0600

    include cals-committee page

--- js/chairs.js
+++ js/chairs.js
@@ -3,8 +3,10 @@ $( document ).ready(function() {
 	//console.log('every page');
 
 	if($('body').is(".page-id-533")){
-	//console.log('it is page-id-533');
+	$("div.entry-content").html('<object data="http://www.cals.wisc.edu/departments/office-of-dean-and-director-2/cals-committees/">')
+	console.log('it is page-id-533');
 	//$("div.entry-content").load("")
+	
 	}
 
 }); 
