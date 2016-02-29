01272647b12e9605653aca2e84f78ae0b3e8490c
Daniel Dropik <ddropik@wisc.edu>
2016-02-29 11:25:49 -0600

    Change post id to "5" to match prod

--- js/chairs.js
+++ js/chairs.js
@@ -2,10 +2,10 @@ $( document ).ready(function() {
 
 	//console.log('every page');
 
-	if($('body').is(".page-id-533")){
+	if($('body').is(".page-id-5")){
 		//load chairs section content from cals committees page
  		$("div.entry-content").load( "http://www.cals.wisc.edu/departments/office-of-dean-and-director-2/cals-committees/ #chairs-content" );
 
 	}
 
-}); 
+});
