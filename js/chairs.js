df82451bbf23e71310fc3d6a092be49c8fb4d2ec
Daniel Dropik <ddropik@wisc.edu>
2016-04-04 13:54:59 -0500

    comment out .load
    rung Grunt task to update master.min.js

--- js/chairs.js
+++ js/chairs.js
@@ -2,10 +2,13 @@ $( document ).ready(function() {
 
 	//console.log('every page');
 
+/*
 	if($('body').is(".page-id-5")){
 		//load chairs section content from cals committees page
  		$("div.entry-content").load( "https://www.cals.wisc.edu/departments/office-of-dean-and-director-2/cals-committees/ #chairs-content" );
 
 	}
+
+	*/
 
 });
