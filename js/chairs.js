68fda96c28a4d493e2884e948d6523e58f6bde41
Daniel Dropik <ddropik@wisc.edu>
2016-04-04 15:19:31 -0500

    comment out .load

--- js/chairs.js
+++ js/chairs.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 
 	if($('body').is(".page-id-5")){
 		//load chairs section content from cals committees page
- 		$("div.entry-content").load( "https://www.cals.wisc.edu/departments/office-of-dean-and-director-2/cals-committees/ #chairs-content" );
+ 	// 	$("div.entry-content").load( "https://www.cals.wisc.edu/departments/office-of-dean-and-director-2/cals-committees/ #chairs-content" );
 
 	}
 
